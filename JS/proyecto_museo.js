1cc2b1d632eef53dfdd8efe412c3f44814daa7c7
Marc Lima <marc.calzifer@gmail.com>
2018-01-26 23:48:13 -0600

    changes add binder sound and play and stop in JS

--- JS/proyecto_museo.js
+++ JS/proyecto_museo.js
@@ -1,7 +1,17 @@
 /**Declarar variables */
 const nosotros = document.getElementById('nosotros');
+const audioMarc = document.getElementById('audioMarc');
+const empieza = document.getElementById('empieza');
+const termina = document.getElementById('termina');
 
 nosotros.addEventListener('click', function () {
     const header = document.getElementById('header');
     header.style.backgroundColor = "#AED581";
+});
+
+empieza.addEventListener('click', function () {
+    audioMarc.play();
+})
+termina.addEventListener('click', function () {
+    audioMarc.load();
 });
