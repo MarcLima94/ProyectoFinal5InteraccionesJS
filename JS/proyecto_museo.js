204267af941a73f5f90138a5cc931823ef68f5f1
Carlos Agustín Lima Aceves <dj.flypo@gmail.com>
2018-01-27 13:29:44 -0600

    Add event listener for decrease 50% when was button clicked.

--- JS/proyecto_museo.js
+++ JS/proyecto_museo.js
@@ -1,8 +1,12 @@
-/**Declarar variables */
+// Cambio de color en header con id='nosotros'.
 const nosotros = document.getElementById('nosotros');
+// Manipular audio
 const audioMarc = document.getElementById('audioMarc');
 const empieza = document.getElementById('empieza');
 const termina = document.getElementById('termina');
+// Animaciones tarjetas.
+const actividades = document.getElementById('actividades');
+const tarjetas = document.getElementsByClassName('size');
 
 nosotros.addEventListener('click', function () {
     const header = document.getElementById('header');
@@ -12,6 +16,15 @@ nosotros.addEventListener('click', function () {
 empieza.addEventListener('click', function () {
     audioMarc.play();
 })
+
 termina.addEventListener('click', function () {
     audioMarc.load();
+});
+
+actividades.addEventListener('click', function() {
+	const arrTarjetas = Array.prototype.slice.call(tarjetas);
+	arrTarjetas.forEach(function(element) {
+		element.style.width = '50%';
+		element.style.height = '50%';
+	})
 });
